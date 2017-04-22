ee2a13f550f175518be31fcc0b6e7d3eefde3d4d
Nick Romney <nickromney@hotmail.com>
2017-04-22 21:06:16 +0100

    simpler test

--- test/bps.js
+++ test/bps.js
@@ -3,23 +3,13 @@ var app = require('../server');
 const expect = require('chai').expect;
 var request = require('supertest');
 
-
-describe('information at root directory of server',
-  function() {
-  it('is connecting locally', function(done) {
-  // pass in our server to supertest
-  request(app)
-    .get('/')
-    // test passes if statusCode is 200
-    .expect(200, function(err, data) {
-      // display error in terminal 
-      console.log('error: ', err);
-      // data is everything we get back from the server
-      console.log('data: ', data);
-    })
-    // test will timeout without end
-    .end(done);
+describe('Availability test', function() {
+  it('should return a status of 200 OK', function(done) {
+    request(app)
+      .get('/')
+      .end(function(err, res) {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
   });
-
 });
-
