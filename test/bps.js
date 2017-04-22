26844dda1b1e7688adcc5f945301052864551b1a
Nick Romney <nickromney@hotmail.com>
2017-04-22 22:03:08 +0100

    Remove chai expect

--- test/bps.js
+++ test/bps.js
@@ -1,14 +1,12 @@
 var app = require('../server');
-// Chai expect
-const expect = require('chai').expect;
 var request = require('supertest');
 
-describe('Availability test', function() {
-  describe('GET root page', function(){
-    it('expects a status of 200 OK', function(done){
+describe('Availability test', function () {
+  describe('GET root page', function () {
+    it('expects a status of 200 OK', function (done) {
       request(app)
-      .get('/')
-    .expect(200, done); 
+        .get('/')
+        .expect(200, done);
     });
   });
 });
