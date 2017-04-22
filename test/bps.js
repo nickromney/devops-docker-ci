da0dc8248b454c3f3e1bcba17db92ef553e5b899
Nick Romney <nickromney@hotmail.com>
2017-04-22 23:03:58 +0100

    Supertest at 3.0.0

--- test/bps.js
+++ test/bps.js
@@ -1,12 +1,15 @@
 var app = require('../server');
+// Chai expect
+const expect = require('chai').expect;
 var request = require('supertest');
 
-describe('Availability test', function () {
-  describe('GET root page', function () {
-    it('expects a status of 200 OK', function (done) {
-      request(app)
-        .get('/')
-        .expect(200, done);
-    });
+describe('Availability test', function() {
+  it('should return a status of 200 OK', function(done) {
+    request(app)
+      .get('/')
+      .end(function(err, res) {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
   });
 });
